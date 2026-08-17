ac7a3aa472f75e47fccd529b6bb03f81a69d9292
Thiago Rissutto <trissutto@gmail.com>
2026-08-17 20:22:06 -0300

    fix(home): usa fotos reais nas novas categorias

--- ecommerce/src/data/home.ts
+++ ecommerce/src/data/home.ts
@@ -5,9 +5,9 @@ export const HOME_CATEGORY_BASE: Array<Omit<HomeCategory, 'href'> & { path: stri
   { name: 'Blusas', image: '/images/home-categorias/blusas.webp', path: '/categoria/blusas', alt: 'Modelo plus size usando blusa creme elegante' },
   { name: 'Conjuntos', image: '/images/home-categorias/conjuntos.webp', path: '/categoria/conjuntos', alt: 'Modelo plus size usando conjunto terracota' },
   { name: 'Calças', image: '/images/home-categorias/calcas.webp', path: '/categoria/calcas', alt: 'Modelo plus size usando calça de alfaiataria' },
-  { name: 'Macacões', image: '/categorias/macacoes.webp', path: '/categoria/macacoes', alt: 'Moda plus size em macacão' },
-  { name: 'Lingerie', image: '/categorias/lingerie.webp', path: '/categoria/lingerie', alt: 'Lingerie plus size' },
-  { name: 'Moda praia', image: '/categorias/moda-praia.webp', path: '/categoria/moda-praia', alt: 'Moda praia plus size' },
+  { name: 'Macacões', image: 'https://pub-84da472609374e0ab161fd54571b5f38.r2.dev/produtos/900751/VERDE/1786069677950-900751-VERDE-1.jpg', path: '/categoria/macacoes', alt: 'Modelo plus size usando macacão verde' },
+  { name: 'Lingerie', image: 'https://pub-84da472609374e0ab161fd54571b5f38.r2.dev/produtos/350842/CHOCOLATE/1786065808991-350842-CHOCOLATE-1-jpg.jpg', path: '/categoria/lingerie', alt: 'Modelo plus size usando cinta modeladora chocolate' },
+  { name: 'Moda praia', image: 'https://pub-84da472609374e0ab161fd54571b5f38.r2.dev/produtos/617581/PRETO/1786066584170-617581-PRETO-1.jpg', path: '/categoria/moda-praia', alt: 'Modelo plus size usando maiô preto' },
   { name: 'Outlet', image: '/images/home-categorias/outlet.webp', path: '/outlet', alt: 'Modelo plus size usando vestido vinho elegante' },
 ];
 
